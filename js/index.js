23484ec2d957611bb6e8f6ffe8e5018852ee1803
Sean Mooney <smooney914@gmail.com>
2017-08-29 18:27:49 -0400

    php contact form added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function($) {
     $("#arrow-down").click(function(event){
         $('html,body').animate({
-        	scrollTop:$("#about").offset().top - 25
+        	scrollTop:$("#about").offset().top - 10
         }, 750);
+        document.location.hash = "";
     });
 });
