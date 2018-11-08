1455769c5b468c5bc9a2bebea98241c8ff77da8f
Sean Mooney <smooney914@gmail.com>
2018-11-07 19:00:30 -0600

    finish portfolio styling

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,6 +2,10 @@ $(document).on("click", ".thumbnail, .thumbnail-tall", function(){
   $('html, body').css('overflow', 'hidden');
 });
 
-$(document).on("click", ".lightbox", function(){
+$(document).on("click", ".lightbox", function(el){
+  console.log(el);
+  if (el.target.classList.contains('left') || el.target.classList.contains('right') || el.target.classList.contains('fa-angle-left') || el.target.classList.contains('fa-angle-right')) {
+    return;
+  }
   $('html, body').css('overflow', 'auto');
 });
