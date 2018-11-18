c5f9aac8f3d5663681399c0401b4056ad6dfe857
Sean Mooney <smooney914@gmail.com>
2018-11-18 10:37:18 -0600

    Home page update desktop only

--- js/index.js
+++ js/index.js
@@ -1,8 +1,41 @@
 jQuery(document).ready(function($) {
     $("#arrow-down").click(function(event){
         $('html,body').animate({
-        	scrollTop:$("#about").offset().top - 10
+            scrollTop:$("#about").offset().top - 10
         }, 750);
         document.location.hash = "";
     });
+    
+});
+var currentCard = null;
+
+function flipCard(card, type) {
+    if (type) {
+        switch(type) {
+            case "javascript":
+            alert('i know javascript');
+            break;
+            default:
+            console.log('unknown type');
+            break;
+        }
+        return;
+    }
+    if (currentCard !== null) {
+        currentCard.classList.toggle('rotate-card');
+        if (currentCard === card) {
+            currentCard = null;
+            return;
+        }
+    }
+    currentCard = card;
+    currentCard.classList.toggle('rotate-card');
+}
+
+$(document).on("click", ".thumbnail", function () {
+    $('body').css('overflow', 'hidden');
+});
+
+$(document).on("click", ".lightbox", function () {
+    $('body').css('overflow', 'auto');
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,8 @@
 $(document).on("click", ".thumbnail, .thumbnail-tall", function(){
-  $('html, body').css('overflow', 'hidden');
+  $('body').css('overflow', 'hidden');
 });
 
 $(document).on("click", ".lightbox", function(el){
-  console.log(el);
   if (el.target.classList.contains('left') || el.target.classList.contains('right') || el.target.classList.contains('fa-angle-left') || el.target.classList.contains('fa-angle-right')) {
     return;
   }
